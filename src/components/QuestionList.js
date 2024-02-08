efe60fe0aab55a83c94a51cfdfd9c607d275274f
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-08 10:02:30 +0300

    completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  // This is where I will display the qsns
+  const [questions, setQuestions] = useState('');
+
+  // Using useEffect to fetch the data to be displayed from the server
+  useEffect(() => {
+    // Fetching the data from the server
+    fetch("http://localhost:4000/questions")
+      // Converting the data to json format
+      .then((response) => response.json())
+      // The data to be dsiplayed after fetching
+      .then((question) => setQuestions(question))
+  },[]); // Empty array dependancy since i just want to render the questions once and for all
+
+  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>  </ul>
     </section>
   );
 }
