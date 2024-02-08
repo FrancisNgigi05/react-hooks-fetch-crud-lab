6a7e1a478a37b0b5c4f1d19acb381bd066cdb74c
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-02-08 17:38:21 +0300

    completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDelete, onChangeOfAnswer }) {
+  const { id, prompt, answers = [], correctIndex } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +9,19 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleAnswerChange(event) {
+    onChangeOfAnswer(id, parseInt(event.target.value));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => onDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  // This is where I will display the qsns
-  const [questions, setQuestions] = useState('');
+  // This is where I will display the questions
+  const [questions, setQuestions] = useState([]);
 
   // Using useEffect to fetch the data to be displayed from the server
   useEffect(() => {
@@ -11,16 +11,59 @@ function QuestionList() {
     fetch("http://localhost:4000/questions")
       // Converting the data to json format
       .then((response) => response.json())
-      // The data to be dsiplayed after fetching
-      .then((question) => setQuestions(question))
-  },[]); // Empty array dependancy since i just want to render the questions once and for all
+      // The data to be displayed after fetching
+      .then((quizes) => setQuestions(quizes))
+  },[]); // Empty array dependency since i just want to render the questions once and for all
 
-  
+  // Mapping over the
+  // Function that will handle the deletion of question
+  function handleDelete(id) {
+    // Fetching data from the server
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE", // Informing the server that I want to delete 
+    })
+    // Converting the data from the server to json format
+      .then((response) => response.json())
+      .then(() => {
+        // return false if the button clicked in the question matches the id of the question
+        const updatedQuizes = questions.filter((quiz) => {
+          return (quiz.id !== id);
+        })
+        // Setting the state of original questions to the updated questions
+        setQuestions(updatedQuizes);
+      })
+  }
+
+  function handleChangeOfAnswer(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH", // Updating a part of the data
+      "Content-Type": "application/json",
+      body: JSON.stringify({ correctIndex }) // stringifying the data to be updated
+    })
+    // Converting the data to be used to json format
+      .then((response) => response.json())
+      .then((updatedQuiz) => {
+        const updatedQuizes = questions.map((quiz) => {
+          if(quiz.id === updatedQuiz.id) {
+            return (updatedQuiz);
+          }
+          else {
+            return (quiz);
+          }
+        });
+        setQuestions(updatedQuizes);
+      })
+  }
+  const questionItems = questions.map((oneQuestion) => {
+    return (
+      <QuestionItem key={oneQuestion.id} question={oneQuestion} onDelete={handleDelete} onChangeOfAnswer={handleChangeOfAnswer}/>
+    )
+  });
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>  </ul>
+      <ul> { questionItems } </ul>
     </section>
   );
 }
